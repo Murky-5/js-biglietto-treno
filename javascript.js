e46f3fdae427a8ab3d31ee8fb070d648e84ce529
MirkoDD <mirkodidio111@gmail.com>
2024-11-28 16:42:54 +0100

    added:user_input_ceck,age_requirement_for_coupons_and_calculated_the_ticket_price

--- javascript.js
+++ javascript.js
@@ -13,7 +13,21 @@
 4 - in fine lo informo di quanto gli costerebbe percorrere i suddetti kilometri.
 */
 
-let userRoute = Number(prompt("inserisci la distanza che devi percorrere (in KM)"));
-let userAge = Number(prompt("ok, ti interessano gli sconti? spero di si... quanto giovane sei? (in anni)"));
+const userRoute = Number(prompt("inserisci la distanza che devi percorrere (in KM)"));
+const userAge = Number(prompt("ok, ti interessano gli sconti? spero di si... quanto giovane sei? (in anni)"));
 const kmValue = Number( userTrace * 0.21 );
+let ticketPrice;
+
+
+if (!userAge || !userRoute) {
+    prompt("hai sicuramente messo uno spazio o delle lettere, questa è robba sseria non cinesate. Riaggiorna la pagina e reinserisci tutto da capo...");
+}
+ else if (userAge > 17) {
+    ticketPrice = kmValue - kmValue * 20 / 100;
+}
+ else if (userAge > 64) {
+    ticketPrice = kmValue - kmValue * 40 / 100;
+}
+
+
 
